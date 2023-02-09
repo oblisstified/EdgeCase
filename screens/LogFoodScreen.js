77d7f18097b168ed8bc8972be1fd8b37cb5b8a02
Stav Armoni-Friedmann <stav.armoni-friedmann@gmail.com>
2023-02-09 20:58:00 +0000

    Add comments

--- screens/LogFoodScreen.js
+++ screens/LogFoodScreen.js
@@ -25,8 +25,9 @@ const LogFoodScreen = ({route, navigation}) => {
 
         for(let i = 0; i < 100 && i < myFoods.length; i++){
 
-            // TODO: Write a custom component that takes a food object as an argument and renders it nicely
+            // passes a food object to display, along with a button to show the modal on this screen
             tempMatches.push(<FoodView key={ myFoods[i]["Description"] } foodDetails={ myFoods[i] } 
+                                        // these buttons are rendered in the FoodView component
                                         button= {<View style={{flexDirection:"row", flex:1, alignSelf: "center"}}>
                                                     <Button title='i' />
                                                     <Button title="Add" onPress={() => {setModalContent(myFoods[i]); setModalVisible(true)}} />
@@ -38,13 +39,18 @@ const LogFoodScreen = ({route, navigation}) => {
 
     return(
         <View style={{flex:1, alignContent: "center", justifyContent: "center"}}>
+
+            {/* search bar */}
             <Text>Food Screen</Text>
             <TextInput onChangeText={ (text) => {setSearchValue(text); getMatches()} } />
             <Button title="search" onPress={ getMatches } />
+
+            {/* list of matches */}
             <ScrollView>
                 { matches }
             </ScrollView>
 
+            {/* popup allowing foodLogging */}
             <Modal
                 animationType="slide"
                 transparent={true}
@@ -53,6 +59,7 @@ const LogFoodScreen = ({route, navigation}) => {
                 Alert.alert('Modal has been closed.');
                 setModalVisible(!modalVisible);
             }}>
+                
                 <FoodLogModal foodDetails={modalContent} hideButton={<Button title="Hide Modal" onPress={() => {setModalVisible(false)}} />} />
             </Modal>
 
--- screens/components/FoodLogModal.js
+++ screens/components/FoodLogModal.js
@@ -8,8 +8,16 @@ const FoodLogModal = props => {
     return (
             <View style={styles.centeredView}>
             <View style={styles.modalView}>
+
+                {/* props.foodDetails contains a json object with all the details about the food shown */}
                 <Text style={styles.modalText}>{props.foodDetails["Description"]}</Text>
+
+                {/* button passed in through props to enable the button to hide */}
                 {props.hideButton}
+
+                {/* TODO: add functionality to input a amount in grams of a Food and store as a json object */
+                /* probably worth writing a function in utils that handles this */}
+
             </View>
             </View>
     );
