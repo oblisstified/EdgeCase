6403608905fdfff74df851848e0be9761eb031c7
k19079055 <PhantasmicEasrl1215>
2023-02-11 03:12:18 +0000

    Add friend button adds user to friends list in the database

--- screens/AllUsersScreen.js
+++ screens/AllUsersScreen.js
@@ -1,47 +1,82 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import 'react-native-gesture-handler';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 import { TextInput } from "react-native-gesture-handler";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getAuth, signOut } from 'firebase/auth';
-import { collection, getDocs} from 'firebase/firestore/lite';
+import { collection, getDocs,updateDoc,doc,getDoc} from 'firebase/firestore/lite';
+import {ref, set} from  'firebase/database';
 import { db } from "../firebase";
 
 import BottomBar from "./components/BottomBar";
 
 const AllUsersScreen = ({route, navigation}) => {
-    const user = getAuth().currentUser;
+  const user = getAuth().currentUser;
+  const [userList, setUserList] = useState([]);
 
-    const getData = async ()=>{
-        const usersCol = collection(db, 'users'); //user collection
-        const userSnapshot = await getDocs(usersCol); //gets all docs from the collection
-        const userList = userSnapshot.docs.map(doc => doc.data());
-        
-        let userEmails = userList.map(({age})=>age); //this gives list of all users' age in the database
-        console.log(userEmails);
 
+  async function updateFriendsList(email){
+    try {
+        const userRef = doc(db, 'users', user.email);
+        const userSnapshot = await getDoc(userRef);
+        const userData = userSnapshot.data();
+        const updatedFriendsList = [...userData.friends, email];
+        await updateDoc(userRef, { friends: updatedFriendsList });
+      } catch (error) {
+        console.error(error);
+      }
+  
+
+    
+
+  }
+
+  useEffect(() => {
+    async function getData () {
+      const usersCol = collection(db, 'users'); //user collection
+      const userSnapshot = await getDocs(usersCol); //gets all docs from the collection
+      const userList = userSnapshot.docs.map(doc => doc.data());
+      setUserList(userList);
     }
 
-    return(
-        <View>
-            <Text>hello {user.email}</Text>
-            <Button title="get Data" onPress={getData}> </Button>
+    getData();
+  }, []);
 
-            {userList.map(item => (
-                <View >
-                    <Text>{item.email}</Text>
+  return(
+    <View>
+      <Text>hello {user.email}</Text>
+
+      {userList.map(item => (
+        <View  style = {styles.userItem}key = {item.email} >
+          <Text>{item.email}</Text> 
+          {/* change to username later */}
+            <TouchableOpacity onPress={() => updateFriendsList(item.email)}>
+                <View>
+                    <Text style={styles.customText}> Add Friend </Text>
                 </View>
-        ))}
-
-
-            <BottomBar />     
+            </TouchableOpacity>
         </View>
-    )
+      ))}
+
+      <BottomBar />     
+    </View>
+  )
 }
 
 export default AllUsersScreen;
 
-
 const styles = StyleSheet.create({
+    userItem:{
+        flexDirection:"row",
+        padding: 10,
+        margin:10,
+        borderWidth:1,
+        borderRadius:10,
+        justifyContent:"space-between",
+    },
+
+    customText:{
+        color:"red", //idk
+    }
  
 })
