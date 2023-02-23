9b5077ee95237b57e49cdf060ee85e5304de6282
Kafi <ahmed.kafi@outlook.com>
2023-02-23 18:13:43 +0000

    dowload uuid and random values

--- App.js
+++ App.js
@@ -13,6 +13,8 @@ import Page1 from './screens/build-profile/Page1';
 import Page2 from './screens/build-profile/Page2';
 import Page3 from './screens/build-profile/Page3';
 import Page4 from './screens/build-profile/Page4';
+import 'react-native-get-random-values';
+import { v4 as uuidv4 } from 'uuid';
 
 
 
