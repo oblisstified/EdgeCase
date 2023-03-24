e65d82a3010186ad6cc0ba0944b8d1f9b689b471
Stav Armoni-Friedmann <stav.armoni-friedmann@kcl.ac.uk>
2023-03-24 10:59:50 +0000

    cleanup ui for foodBasketScreen

--- screens/FoodBasketScreen.js
+++ screens/FoodBasketScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import 'react-native-gesture-handler';
-import { StyleSheet, Text, View, Button, DeviceEventEmitter, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, DeviceEventEmitter, TextInput, SafeAreaView, TouchableOpacity } from 'react-native';
 import { useNavigation } from "@react-navigation/native";
 
 import { getAuth } from 'firebase/auth'
@@ -58,24 +58,145 @@ const FoodBasketScreen = props => {
     }
 
     return (
-        <View testID="basketList">
+        <SafeAreaView>
+            <View style={{marginTop:40, marginBottom:40}} testID="basketList">
+            <View style={{height:300}}>
             { food && food.map((o) => o &&
-                <View key={o.foodObject.Description}>
-                    <Text>
-                        {o.foodObject.Description + ": " + o.weight}
-                    </Text>
-                    <Button testID={o.foodObject.Description + "button"} title="remove" onPress={()=>handleRemove(o)}/>
+                <View style={[{flex: 1, width: "100%", height:40, alignItems:"center", flexDirection:"row"}]} key={o.foodObject.Description}>
+                    <Text style={{padding:15}}> {
+                        o.foodObject["Description"].toLowerCase() // makes every letter lowercase
+                                                    .replace(/,/g, ", ") // replaces every "," with a ", "
+                                                    .replace(/(^\w{1})|(\s+\w{1})/g, (letter) => letter.toUpperCase())
+                    } </Text>
+                    <TouchableOpacity
+                        testID={o.foodObject.Description + "button"} 
+                        onPress={
+                            () => handleRemove(o)
+                        }
+                        style={[
+                        styles.shadowProp,
+                        {
+                            backgroundColor: "#00a46c",
+                            paddingHorizontal: 20,
+                            paddingVertical: 5,
+                            borderRadius: 15,
+                            marginRight: 15,
+                            marginTop: 15,
+                        },
+                        ]}
+                    >
+                        <Text
+                        style={{
+                            fontWeight: "bold",
+                            fontSize: 12,
+                            color: "#FFF",
+                            textAlign: "center",
+                        }}
+                        >
+                        Remove Ingredient
+                        </Text>
+                        
+                    </TouchableOpacity>
                 </View>
-            ) }
+            )}
+            </View>
+            <TouchableOpacity
+                    testID="saveButton"
+                    onPress={() => {
+                        onPress={saveBasket}
+                    }}
+                    style={[
+                      styles.shadowProp,
+                      {
+                        backgroundColor: "#00a46c",
+                        paddingHorizontal: 20,
+                        paddingVertical: 5,
+                        borderRadius: 15,
+                        marginRight: 15,
+                        marginTop: 15,
+                      },
+                    ]}
+                  >
+                    <Text
+                      style={{
+                        fontWeight: "bold",
+                        fontSize: 26,
+                        color: "#FFF",
+                        textAlign: "center",
+                      }}
+                    >
+                      Save As Meal
+                    </Text>
+                  </TouchableOpacity>
 
-            <Button  testID="saveButton" title="save" onPress={saveBasket} />
-            <Button testID="savePresetButton" title="Save Preset" onPress={savePreset} />
+            <TouchableOpacity
+                    testID="savePresetButton"
+                    onPress={() => {
+                        onPress={savePreset}
+                    }}
+                    style={[
+                      styles.shadowProp,
+                      {
+                        backgroundColor: "#00a46c",
+                        paddingHorizontal: 20,
+                        paddingVertical: 5,
+                        borderRadius: 15,
+                        marginRight: 15,
+                        marginTop: 15,
+                      },
+                    ]}
+                  >
+                    <Text
+                      style={{
+                        fontWeight: "bold",
+                        fontSize: 26,
+                        color: "#FFF",
+                        textAlign: "center",
+                      }}
+                    >
+                      Save Preset
+                    </Text>
+                  </TouchableOpacity>
             <TextInput testID="nameBar" placeholder="preset name" onChangeText={setDescription}/>
             {presetNameError && <Text style={{color:"red"}}>Preset needs a name to be saved</Text> }
             {presetSaved && <Text style={{color:"green"}}>Preset saved!</Text> }
 
-        </View>
+            </View>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    header: {
+      fontSize: 14,
+      paddingTop: 5,
+      marginHorizontal: 5,
+      marginTop: 12,
+    },
+    details: {
+      fontSize: 12,
+      opacity: 0.6,
+      marginHorizontal: 5,
+      paddingBottom: 5,
+      marginTop: 5,
+    },
+    displayInfo: {
+      flex: 1,
+      flexDirection: "row",
+      marginHorizontal: 5,
+      marginVertical: 5,
+      borderRadius: 15,
+      backgroundColor: "#ededed",
+      paddingLeft: 5,
+      height: 70,
+    },
+    shadowProp: {
+        shadowColor: "#171717",
+        shadowOffset: { width: -4, height: 4 },
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+      },
+  });
+  
 
 export default FoodBasketScreen
